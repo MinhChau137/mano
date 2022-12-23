f4b17f936a57d9e249982edf044c78d8945df3f5
minhChau137 <c0963018619@gmail.com>
2022-12-24 01:19:26 +0700

    v1

---
+++ src/layouts/Main/MainLayout.js
@@ -0,0 +1,13 @@
+import Menu from '../../components/Menu/Menu';
+
+
+const MainLayout = () => {
+  
+  return (
+    <>
+      <Menu/>
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet';
+
+const HomePage = () => {
+  return(
+    <>
+      <Helmet>
+        <title>Home | Mano</title>
+      </Helmet>
+    </>
+  )
+}
+export default HomePage;
